c97afd5fd0d880dd4343940463eefeba682cfd56
YINGYYUN <756209834@qq.com>
2026-01-24 12:05:08 +0800

    feat:暂存一个失败的版本

--- project/code/Key.c
+++ project/code/Key.c
@@ -15,20 +15,21 @@ void Key_Init(void)
     gpio_init(KEY1, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY1 输入 默认高电平 上拉输入
     gpio_init(KEY2, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY2 输入 默认高电平 上拉输入
     gpio_init(KEY3, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY3 输入 默认高电平 上拉输入
-    gpio_init(KEY4, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY4 输入 默认高电平 上拉输入	
+    gpio_init(KEY4, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY4 输入 默认高电平 上拉输入
+	
 }
 
 //外部调用，查看被按下的按键
 uint8_t Key_GetNum(void)
 {
 	uint8_t Temp;
-	if(Key_Num)
-	{
+//	if(Key_Num)//防止可能由中断造成的标志位直接清零，致使按键事件被忽略
+//	{
 		Temp = Key_Num;//利用中间变量实现标志位的返回并清零
 		Key_Num = 0;
 		return Temp;
-	}
-	return 0;
+//	}
+//	return 0;
 }
 
 
@@ -60,7 +61,7 @@ void Key_Tick(void)//利用定时中断调用，获取通用的时间基准
 	//静态变量默认值为0，函数退出后值不会丢失
 	
 	Count++;//计数分频
-	if(Count>=2)
+	if(Count>=20)
 	{
 		Count=0;
 		
--- project/user/src/main.c
+++ project/user/src/main.c
@@ -38,6 +38,8 @@
 #include "zf_device_oled.h"
 #include "zf_driver_timer.h"
 
+#include "Key.h"
+
 
 // 打开新的工程或者工程移动了位置务必执行以下操作
 // 第一步 关闭上面所有打开的文件
@@ -45,67 +47,86 @@
 
 // 本例程是开源库移植用空工程
 
-#define KEY1                    (E2 )
-#define KEY2                    (E3 )
-#define KEY3                    (E4 )
-#define KEY4                    (E5 )
-
 #define SWITCH1                 (D3 )
 #define SWITCH2                 (D4 )
+
+#define PIT                     (TIM6_PIT )                                     // 使用的周期中断编号 如果修改 需要同步对应修改周期中断编号与 isr.c 中的调用
+#define PIT_PRIORITY            (TIM6_IRQn)                                     // 对应周期中断的中断编号 在 mm32f3277gx.h 头文件中查看 IRQn_Type 枚举体
+
+volatile uint8_t pit_state = 0;
+
+//初始为无效值0
+volatile uint8_t KeyNum = 0;
 
 // **************************** 代码区域 ****************************
 int main(void)       
 {
-	clock_init(SYSTEM_CLOCK_120M);                                              // 初始化芯片时钟 工作频率为 120MHz
+    clock_init(SYSTEM_CLOCK_120M);                                              // 初始化芯片时钟 工作频率为 120MHz
     debug_init();                                                               // 初始化默认 Debug UART
 
-	//OLED初始化
-	oled_init();
-	//另一个可以使用的字体为8X16,但是坐标仍然是基于6X8标定的
-	oled_set_font(OLED_6X8_FONT);	
-	
-	//按键初始化
-	gpio_init(KEY1, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY1 输入 默认高电平 上拉输入
-	gpio_init(KEY2, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY2 输入 默认高电平 上拉输入
-	gpio_init(KEY3, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY3 输入 默认高电平 上拉输入
-	gpio_init(KEY4, GPI, GPIO_HIGH, GPI_PULL_UP);                               // 初始化 KEY4 输入 默认高电平 上拉输入
+    //OLED初始化
+    oled_init();
+    //另一个可以使用的字体为8X16,但是坐标仍然是基于6X8标定的
+    oled_set_font(OLED_6X8_FONT);    
+    
+	Key_Init();
 
-	//拨码开关初始化
-	gpio_init(SWITCH1, GPI, GPIO_HIGH, GPI_FLOATING_IN);                        // 初始化 SWITCH1 输入 默认高电平 浮空输入
-	gpio_init(SWITCH2, GPI, GPIO_HIGH, GPI_FLOATING_IN);                        // 初始化 SWITCH2 输入 默认高电平 浮空输入
-	
-	
+//    //拨码开关初始化
+//    gpio_init(SWITCH1, GPI, GPIO_HIGH, GPI_FLOATING_IN);                        // 初始化 SWITCH1 输入 默认高电平 浮空输入
+//    gpio_init(SWITCH2, GPI, GPIO_HIGH, GPI_FLOATING_IN);                        // 初始化 SWITCH2 输入 默认高电平 浮空输入
+    
 	uint16_t i = 0, j = 0;
 	
+	pit_ms_init(PIT, 10);                                                     // 初始化 PIT 为周期中断 10ms 周期
+
+    interrupt_set_priority(PIT_PRIORITY, 0);                                    // 设置 PIT 对周期中断的中断优先级为 0
+        
     while(1)
     {
-			
-		//按键按下电平为0
-		i = 0, j = 0;
-		if (gpio_get_level(KEY4))
-		{
-			i = 1;
-		}
-		if (!gpio_get_level(KEY4))
-		{
-			j = 1;
-		}
-			
-			
-		//拨码开关上0，下一		
-//		if(gpio_get_level(SWITCH1))
-//		{
-//			i = 1;
-//		}
-//		if(!gpio_get_level(SWITCH1))
-//		{
-//			j = 1;
-//		}
-			
-			
+        KeyNum = Key_GetNum();
+		
+		j = 0;
+//        //按键按下电平为0
+        if (gpio_get_level(E2))
+        {
+            j = 1;
+        }
+//            
+//            
+//        //拨码开关拨下为高电平，拨上为低电平        
+//        if(gpio_get_level(SWITCH2))//1
+//        {
+//            i = 1;
+//        }
+//        if(!gpio_get_level(SWITCH2))//0
+//        {
+//            j = 1;
+//        }              
+				
+		if(pit_state)
+        {
+			i ++;		
+            pit_state = 0;                                                      // 清空周期中断触发标志位
+			Key_Tick();
+        }
+		
+
 		oled_show_uint(0, 0, i, 4);
 		oled_show_uint(0, 1, j, 4);
-				
+		oled_show_uint(0, 3, KeyNum, 4);
     }
 }
+
+//-------------------------------------------------------------------------------------------------------------------
+// 函数简介     PIT 的周期中断处理函数 这个函数将在 PIT 对应的定时器中断调用 详见 isr.c
+// 参数说明     void
+// 返回参数     void
+// 使用示例     pit_handler();
+// 备注信息
+//-------------------------------------------------------------------------------------------------------------------
+void pit_handler (void)
+{
+    pit_state = 1;                                                              // 周期中断触发 标志位置位
+}
+
 // **************************** 代码区域 ****************************
